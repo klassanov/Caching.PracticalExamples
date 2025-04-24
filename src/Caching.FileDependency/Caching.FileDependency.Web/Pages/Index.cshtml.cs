2a6a440342d9e785422c67f3419bf533e8a6130e
Alexander Klassanov <klassanov@live.com>
2025-04-24 18:56:12 +0300

    Filechange token dependency added

--- src/Caching.FileDependency/Caching.FileDependency.Web/Pages/Index.cshtml.cs
+++ src/Caching.FileDependency/Caching.FileDependency.Web/Pages/Index.cshtml.cs
@@ -1,28 +1,61 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.FileProviders;
 
 namespace Caching.FileDependency.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        
+        private const string fileKey = "FileKey";
+        private readonly ILogger<IndexModel> logger;
+        private readonly IMemoryCache cache;
+        private readonly IFileProvider fileProvider;
 
         [BindProperty]
         public string FilePath { get; set; } = @"C:\tmp\test.txt";
 
 
         [BindProperty]
-        public string FileContent { get; set; } = string.Empty;
+        public string FileContent { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IMemoryCache cache, ILogger<IndexModel> logger)
         {
-            _logger = logger;
+            this.cache = cache;
+            this.logger = logger;
+            this.fileProvider = new PhysicalFileProvider(@"C:\tmp");
         }
 
         public async Task OnGet()
         {
-            FileContent = await System.IO.File.ReadAllTextAsync (FilePath);
+            if (cache.TryGetValue(fileKey, out string? cachedContent))
+            {
+                FileContent = cachedContent!;
+                logger.LogInformation("Cache hit: File Content Loaded from the cache");
+            }
+            else
+            {
+                FileContent = await System.IO.File.ReadAllTextAsync(FilePath);
+                logger.LogInformation("Cache miss: File Content Loaded from the file");
+                AddFileContentToCache();
+            }
+        }
+
+        private void AddFileContentToCache()
+        {
+            var relativePath = Path.GetRelativePath(@"C:\tmp", FilePath);
+            var changeToken = fileProvider.Watch(relativePath);
+
+            //Combine more than one condition for cache expiration - OR logic
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                   //.SetSlidingExpiration(TimeSpan.FromSeconds(20))
+                   .AddExpirationToken(changeToken)
+                   .RegisterPostEvictionCallback((key, value, reason, state) =>
+                   {
+                       logger.LogInformation("Cache entry '{key}' was evicted. Reason: {reason}", key, reason);
+                   });
+
+            cache.Set(fileKey, FileContent, cacheEntryOptions);
         }
     }
 }
